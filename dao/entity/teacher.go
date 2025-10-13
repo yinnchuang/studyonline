8eed990394eb280ea2279c45b7c8d82a9f140116
evan <yinnchuang@163.com>
2025-10-13 18:47:45 +0800

    feat: username设为unique

--- dao/entity/teacher.go
+++ dao/entity/teacher.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Teacher struct {
 	gorm.Model
 	Name       string `json:"name" gorm:"not null"`
-	Username   string `json:"Username" gorm:"not null"`
+	Username   string `json:"Username" gorm:"not null unique"`
 	Password   string `json:"password" gorm:"not null"`
 	Department string `json:"department" gorm:"not null"`
 }
